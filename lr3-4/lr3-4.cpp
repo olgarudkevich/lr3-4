bae216529406c07b18502b31addd9e79deadd05b
olgarudkevich <olgarudkevich3785@gmail.com>
2025-09-22 13:55:42 +0300

    Add project files.

---
+++ lr3-4/lr3-4.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int n, sum = 0;
+
+    cout << "Введіть натуральне число: ";
+    cin >> n;
+
+    if (n <= 0) {
+        cout << "Число має бути натуральним (більше 0)!" << endl;
+        return 1;
+    }
+
+    // Використовуємо цикл for
+    for (int temp = n; temp > 0; temp /= 10) {
+        sum += temp % 10;
+    }
+
+    cout << "Сума цифр числа " << n << " дорівнює: " << sum << endl;
+
+    return 0;
+}
